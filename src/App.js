d1f234317def06e619b0217d9ffa63774373e799
MZO9400 <hamza.hameed00@yahoo.com>
2020-09-09 10:38:41 +0500

    App.js: Pass dummy data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import JSONcomponent from './components/JSONcomponent';
+
+const JSON = {
+  "random": 73,
+  "random float": 82.985,
+  "bool": false,
+  "date": "1990-03-27",
+  "regEx": "helloooooooooooooooooooooooooooooooo world",
+  "enum": "generator",
+  "firstname": "Jerry",
+  "lastname": "Brodench",
+  "city": "Moscow",
+  "country": "Korea, Democratic People\"S Republic of",
+  "countryCode": "TF",
+  "email uses current data": "Jerry.Brodench@gmail.com",
+  "email from expression": "Jerry.Brodench@yopmail.com",
+  "array": [
+    "Olwen",
+    "Jenilee",
+    "Elyssa",
+    "Malina",
+    "Jolyn"
+  ],
+  "array of objects": [
+    {
+      "index": 0,
+      "index start at 5": 5
+    },
+    {
+      "index": 1,
+      "index start at 5": 6
+    },
+    {
+      "index": 2,
+      "index start at 5": 7
+    }
+  ]
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <JSONcomponent JSON={JSON}/>;
 }
 
 export default App;
